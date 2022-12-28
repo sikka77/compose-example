f10c7ab9e1333185a9f073c9ae734c59a4e96f82
bhavya.sikka <bhavya.sikka@niyotail.com>
2022-12-28 12:25:32 +0530

    app package structure complete

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/commons/Constants.kt
@@ -0,0 +1,8 @@
+package com.plcoding.cryptocurrencyappyt.commons
+
+/**
+ * Created by Bhavya Sikka on 28/12/22.
+ */
+object Constants {
+
+}
---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/data/remote/CoinApiService.kt
@@ -0,0 +1,8 @@
+package com.plcoding.cryptocurrencyappyt.data.remote
+
+/**
+ * Created by Bhavya Sikka on 28/12/22.
+ */
+interface CoinApiService {
+
+}
